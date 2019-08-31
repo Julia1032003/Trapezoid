3bedcbdafa6d56abb748821d8017471612297cce
Julia <julia1032003@gmail.com>
2019-08-31 22:53:58 +0800

    Initial Commit

---
+++ Trapezoid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trapezoid
+//
+//  Created by Julia Wang on 2019/8/31.
+//  Copyright © 2019 Julia Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
